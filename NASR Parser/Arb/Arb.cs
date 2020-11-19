473ac9582be1017aef6d8e1595937128bbad4d77
evgeny.vasilyev <evgeny.vasilyev@i3-corps.com>
2020-11-19 14:45:44 -0600

    Created directories for different Record Types; Created the Arb Record;

---
+++ NASR Parser/Arb/Arb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace NASR_Parser
+{
+    public class Arb
+    {
+        public static List<Arb1Record> Arb1Record { get; } = new List<Arb1Record>();
+
+        public static Aff Parse(string pathToDb)
+        {
+            var pathToFile = Path.Combine(pathToDb, "ARB.txt");
+            var lines = File.ReadAllLines(pathToFile);
+
+            var aff = new Aff();
+
+            foreach (var line in lines)
+            {
+                using (var reader = new StringReader(line))
+                {
+                    var arb1 = RecordParser.Parse<Arb1Record>(reader);
+                    Arb1Record.Add(arb1);
+                }
+            }
+
+            return aff;
+        }
+    }
+}
---
+++ NASR Parser/Arb/Arb1Record.cs
@@ -0,0 +1,35 @@
+﻿namespace NASR_Parser
+{
+    public class Arb1Record
+    {
+        [Field(04)]
+        public string ArtccIdent;
+        
+        [Field(03)]
+        public string AltStructCode;
+
+        [Field(05)]
+        public string PointDesignator;
+
+        [Field(40)]
+        public string ArtccName;
+        
+        [Field(10)]
+        public string AltStructName;
+
+        [Field(14)]
+        public string Latitude;
+        
+        [Field(14)]
+        public string Longitude;
+
+        [Field(300)]
+        public string Description;
+        
+        [Field(06)]
+        public string SequenceNumber;
+        
+        [Field(01)]
+        public string NasDescriptionFlag;
+    }
+}
